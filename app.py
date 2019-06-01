859838ce09e7f3d9683ed7e83e8f4524be420a97
8area8 <mbriolet.ma@gmail.com>
2019-06-01 15:12:00 +0200

    first commit

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    the_time = datetime.now().strftime("%d/%m/%y")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    return ('<h1>Hello heroku</h1>'
+            f'<p>It is currently {the_time}.</p>'
+            '<img src="http://loremflickr.com/600/400" />')
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
